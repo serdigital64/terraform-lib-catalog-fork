e8a12006147217429826202a7085e68d3db7c311
SerDigital64 (Max) <serdigital64@gmail.com>
2026-03-30 09:52:12 +0200

    real: promote 1.2.0 (#2)

---
+++ src/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  #
+  # Shared
+  #
+
+  shared__labels = {
+    at64_shared__automated      = "true"
+    at64_shared__module_type    = "terraform"
+    at64_shared__module_name    = "terraform-lib-catalog"
+    at64_shared__module_version = "1-2-0"
+  }
+}
